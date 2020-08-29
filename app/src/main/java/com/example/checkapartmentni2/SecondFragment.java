b748366caf538136c0c580e1af7ff47bab5d95e1
copito.123 <nicolspnicolsp@gmail.com>
2020-08-28 22:15:15 -0400

    agregados los if

--- app/src/main/java/com/example/checkapartmentni2/SecondFragment.java
+++ app/src/main/java/com/example/checkapartmentni2/SecondFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +19,10 @@ public class SecondFragment extends Fragment {
     private String numero;
     private String direccion;
     private String url;
+    int puntaje=0;
+    int valor=0;
+    int valortotal=0;
+    int puntajetotal=0;
 
 
     private FragmentSecondBinding mBinding;
@@ -33,11 +38,6 @@ public class SecondFragment extends Fragment {
         }
     }
 
-    public void Guardar (){
-
-    }
-
-
 
 
        public View onCreateView(
@@ -47,6 +47,37 @@ public class SecondFragment extends Fragment {
         // Inflate the layout for this fragment
         mBinding = FragmentSecondBinding.inflate(inflater,container,false);
            Glide.with(getContext()).load(url).into(mBinding.i2f);
+           mBinding.rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+               @Override
+               public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                   if (mBinding.tvf4.isChecked()) {
+                       puntaje+= 10;
+                   }
+                   if (mBinding.tvf5.isChecked()) {
+                       puntaje+= 40;
+                   }
+                   if (mBinding.tvf6.isChecked()) {
+                       puntaje+= 30;
+                   }
+                   if (mBinding.tvf7.isChecked()) {
+                       puntaje+= 20;
+                   }
+                   if (mBinding.tvf10.isChecked()) {
+                       valor=3;
+                   }
+                   if (mBinding.tvf11.isChecked()) {
+                       valor=2;
+                   }
+                   if (mBinding.tvf12.isChecked()) {
+                       valor=1;
+                   }
+
+               }
+
+             //  public void EnviarAlerta(
+               //        if(puntaje<130)
+          //     ){}
+           });
 
 
         return mBinding.getRoot();
